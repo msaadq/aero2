3932d059fc06b7858393f3162e83a03cd7e6a6c5
Usman Mahmood <usmanwardag@gmail.com>
2015-11-20 13:51:58 +0500

    ADC configured with smog sensor at PC12

--- AerO2_MCU/Src/main.c
+++ AerO2_MCU/Src/main.c
@@ -1,74 +1,38 @@
 /**
-  ******************************************************************************
-  * File Name          : main.c
-  * Description        : Main program body
-  ******************************************************************************
-  *
-  * COPYRIGHT(c) 2015 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
+
+Aer02:
+
+Pins
+----
+PC2: Connected to smog sensor (ADC1)
+PA10: USART1 RX
+PA9: USART1 TX
+
+Output
+------
+After 1ms, smog sensor's value is sent to bluetooth.
+
+Contributors:
+- Saad
+- Usman
+- Muddassir
+
+*/
+
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_hal.h"
 #include "adc.h"
 #include "usart.h"
 #include "gpio.h"
 
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 
-/* USER CODE BEGIN PFP */
-/* Private function prototypes -----------------------------------------------*/
-
-/* USER CODE END PFP */
-
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration----------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* Configure the system clock */
@@ -78,29 +42,28 @@ int main(void)
   MX_GPIO_Init();
   MX_ADC1_Init();
   MX_USART1_UART_Init();
-	uint32_t value;
 
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Define variables */
+	uint32_t adc_value;		
+	uint32_t sampling_delay = 1000;
+	
+	
   while (1)
   {
-  /* USER CODE END WHILE */
-		HAL_ADCEx_Calibration_Start(&hadc1);
+			
 		HAL_ADC_Start(&hadc1);
-		value = HAL_ADC_GetValue(&hadc1);
-  /* USER CODE BEGIN 3 */
+		adc_value = HAL_ADC_GetValue(&hadc1);
+		//adc_value is converted to uint8_t integer and size is divided by 4
+		HAL_UART_Transmit(&huart1,(uint8_t*)(&adc_value), sizeof(adc_value)/4,1);
+		HAL_ADC_Stop(&hadc1);
+		HAL_Delay(sampling_delay);
 		
   }
-  /* USER CODE END 3 */
-
 }
 
 /** System Clock Configuration
 */
+
 void SystemClock_Config(void)
 {
 
@@ -131,13 +94,10 @@ void SystemClock_Config(void)
 
   HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 
-  /* SysTick_IRQn interrupt configuration */
+  // SysTick_IRQn interrupt configuration 
   HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
 }
 
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
 
 #ifdef USE_FULL_ASSERT
 
@@ -148,6 +108,7 @@ void SystemClock_Config(void)
    * @param line: assert_param error line source number
    * @retval None
    */
+
 void assert_failed(uint8_t* file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
