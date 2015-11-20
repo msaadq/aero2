92a320f0d7bd5faaae7f26ea8c2ced66f737feb6
Usman Mahmood <usmanwardag@gmail.com>
2015-11-20 10:52:36 +0500

    Added new MCU Project files

---
+++ AerO2_MCU/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ADC_1' 
+ * Target:  'ADC_1 Configuration' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
