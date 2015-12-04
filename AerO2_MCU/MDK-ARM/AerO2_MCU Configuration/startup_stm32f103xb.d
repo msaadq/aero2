0f1a52b8164a275567f6ac8f37ce96e8f6e26599
msaadq94 <msaadq94@gmail.com>
2015-12-04 10:52:56 +0500

    Revomed the unnecessary files from git

--- AerO2_MCU/MDK-ARM/AerO2_MCU Configuration/startup_stm32f103xb.d
+++
@@ -1,1 +0,0 @@
-aero2_mcu configuration/\startup_stm32f103xb.o: C:/Users/Saad/STM32Cube/Repository/STM32Cube_FW_F1_V1.2.0/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/arm/startup_stm32f103xb.s
