7cc0245d23f49a7a5dac126ee923ac9bf945a47f
Usman Mahmood <usmanwardag@gmail.com>
2015-12-07 21:10:18 +0500

    Traffic estimate function implemented

--- AerO2_maps_api/maps.py
+++ AerO2_maps_api/maps.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup as bs
 import urllib2
 
 
-def findPlace(key, latitude, longitude, radius, ptype):
+def findPlace(key, origin, radius, ptype):
 
 	'''Calls Google Place API and returns the nearest place within 
 	the radius provided by user'''
 
 	web_link = 'https://maps.googleapis.com/maps/api/place/nearbysearch/xml?key='+key
-	web_link += '&location='+latitude+','+longitude+'&radius='+radius+'&type='+ptype
+	web_link += '&location='+origin[0]+','+origin[1]+'&radius='+radius+'&type='+ptype
 	source = urllib2.urlopen(web_link)
 	tree = bs(source)
 
@@ -38,12 +38,29 @@ def calDuration (key, origin, destination, departure_time='now',traffic_model='b
 
 	return duration/distance
 
+
+def calTraffic (key, origin, increment=0.015):
+
+	'''Estimates traffic by averaging duration over 4 different 
+	directions from origin'''
+
+	destination1 = [str(float(origin[0]) + increment), origin[1]]
+	destination2 = [str(float(origin[0]) - increment), origin[1]]
+	destination3 = [origin[0],str(float(origin[1]) + increment)]
+	destination4 = [origin[0],str(float(origin[1]) - increment)]
+
+	d1 = calDuration(key, origin, destination1)
+	d2 = calDuration(key, origin, destination2)
+	d3 = calDuration(key, origin, destination3)
+	d4 = calDuration(key, origin, destination4)
+	
+	return (d1+d2+d3+d4)/4
+
+
 def main():
 
 	key1 = 'AIzaSyDRhcSUYbhG25wWSKRmvau1GuoXCnnjN8c'
 	key2 = 'AIzaSyDKQ-7d3pejj3BxgdGusj3djD4ppwWtn2s'
-	latitude = '33.6499632'
-	longitude = '72.9637728'
 	origin = ['33.6499632','72.9637728']
 	destination = ['33.6499632','72.9737728']
 	radius = '2000'
@@ -51,14 +68,19 @@ def main():
 
 	'''
 	#Test Code for findPlace function
-
-	results_pl = findPlace(key, latitude, longitude, radius, ptype)
+	results_pl = findPlace(key, origin, radius, ptype)
 	results_pl_text = [i.getText() for i in results]
 	print results_text
 
 	'''
-	
+
+	'''
+	#Test code for calDuration function
 	print calDuration(key2, origin, destination)
+	'''
+
+	print calTraffic(key2,origin)
+# 
 
 if __name__ == '__main__':
 	main()
