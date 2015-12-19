49671475d17f51b5fc304f2a2ad8edf7029ea2bf
msaadq <msaadq94@gmail.com>
2015-12-20 03:28:43 +0500

    Adds the SQL query file

--- Aero2_Backend/src/Main.py
+++ Aero2_Backend/src/Main.py
@@ -1,4 +1,6 @@
-﻿import DataBaseLayer as dbl
+﻿'''
+
+import DataBaseLayer as dbl
 
 database_layer = dbl.DataBaseLayer()
 
@@ -7,3 +9,22 @@ RESULTS_TABLE_NAME = "aero2.SampleDataTable"
 where_string = " smog BETWEEN 4.0 AND 10.0"
 
 print database_layer.delete_data(RESULTS_TABLE_NAME, where_string)
+
+
+'''
+
+def calc(number):
+    sum = 0
+
+    while number / 10 != 0:
+        sum += number % 10
+        number /= 10
+
+    if sum / 10 != 0:
+        sum = calc(sum)
+
+    return sum
+
+print calc(12345)
+        
+        
