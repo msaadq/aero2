944212bccf3d2616778ebcda5857f07b654df35e
msaadq <msaadq94@gmail.com>
2015-12-29 08:14:59 +0500

    Completes the intitialization code

--- Aero2_Backend/src/DataTraining.py
+++ Aero2_Backend/src/DataTraining.py
@@ -1,12 +1,14 @@
-
+import maps
 import DataBaseLayer as dbl
+import math
 
 class DataTraining:
 
     VERIFICATION_THRESHOLD = 0.8
+    _database
 
     def __init__(self):
-        pass
+        self._database = dbl.DataBaseLayer()
 
     def initialize(self, city_name):
         return self._save_all_properties(city_name)
@@ -30,8 +32,8 @@ class DataTraining:
 
         x1, x2, y1, y2 = corner_coordinates[0][1], corner_coordinates[1][1], corner_coordinates[0][0], corner_coordinates[3][0]
 
-        long_interval = abs(x1 - x2) / (self._calc_distance_from_long(x1, x2) / 20.0)
-        lat_interval = abs(y1 - y2) / (self._calc_distance_from_lat(y1, y2) / 20.0)
+        long_interval = abs(x1 - x2) / (self._calc_distance_from_long(x1, x2, y1) / 20.0)
+        lat_interval = abs(y1 - y2) / (self._calc_distance_from_lat(y1, y2, x1) / 20.0)
 
         latitude = y1
         longitude = x1
@@ -43,19 +45,17 @@ class DataTraining:
 
         return all_coordinates
 
-    def _map_properties(self, nodes_coordinates):
+    def _map_properties(self, nodes_coordinates, time_stamps):
         output_table = []
         i = 0
         for single_coordinates in nodes_coordinates:
-            output_table[i][0:2] = single_coordinates
-            output_table[i][3] = maps.get_road_index(single_coordinates)
-            output_table[i][4] = maps.get_industry_index(single_coordinates)
+            output_table.append(single_coordinates, time_stamps[i], maps.get_altitude(single_coordinates), maps.get_road_index(single_coordinates), maps.get_industry_index(single_coordinates))
             i += 1
 
         return output_table
 
     def _save_nodes_properties(self, nodes_properties_table):
-        pass
+        self._database.insert_multiple(self._database.PROP_TABLE_NAME, nodes_properties_table)
 
 
     '''
@@ -96,12 +96,42 @@ class DataTraining:
     def _get_table_output(self, table_without_outputs):
         pass
 
-    def _calc_distance_from_lat(self, lat_min, lat_max):
-        distance = 400
+    def _calc_distance_from_lat(self, lat_min, lat_max, ref_long):
+        return self._calc_distance_on_unit_sphere(lat_min, ref_long, lat_max, ref_long)
 
-        return distance
+    def _calc_distance_from_long(self, long_min, long_max, ref_lat):
+    
+        print ref_lat
+        print long_min
+        print long_max
+        return self._calc_distance_on_unit_sphere(ref_lat, long_min, ref_lat, long_max)
 
-    def _calc_distance_from_long(self, long_min, long_max):
-        distance = 400
+    def _calc_distance_on_unit_sphere(lat1, long1, lat2, long2):
 
-        return distance
+        # Convert latitude and longitude to 
+        # spherical coordinates in radians.
+        degrees_to_radians = math.pi/180.0
+            
+        # phi = 90 - latitude
+        phi1 = (90.0 - lat1)*degrees_to_radians
+        phi2 = (90.0 - lat2)*degrees_to_radians
+            
+        # theta = longitude
+        theta1 = long1*degrees_to_radians
+        theta2 = long2*degrees_to_radians
+            
+        # Compute spherical distance from spherical coordinates.
+            
+        # For two locations in spherical coordinates 
+        # (1, theta, phi) and (1, theta', phi')
+        # cosine( arc length ) = 
+        #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+        # distance = rho * arc length
+        
+        cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
+            math.cos(phi1)*math.cos(phi2))
+        arc = math.acos( cos )
+
+        # Remember to multiply arc by the radius of the earth 
+        # in your favorite set of units to get length.
+        return arc * 6373
--- Aero2_Backend/src/Main.py
+++ Aero2_Backend/src/Main.py
@@ -2,5 +2,5 @@
 
 data_train = dt.DataTraining()
 
-print data_train._get_all_coordinates(([1,1], [1,2], [2,1], [2,2]))
+print data_train._get_all_coordinates([[1,1], [1,2], [2,1], [2,2]])
         
